3b0941709963698a0c65e6ffb61ea87e973d69ab
rogeriocz <rogeriocz@gmail.com>
2021-09-16 07:43:18 -0500

    practicas de vue

---
+++ 03-fundamentos/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <Counter />
+  
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+Counter
+
+export default {
+  name: 'Batman',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Counter</h2>
+  <p>8000</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
