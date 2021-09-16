4e7b1ea29ea26257cb0757624b1fb966cc815e35
rogeriocz <rogeriocz@gmail.com>
2021-09-16 10:12:01 -0500

    se agrega methods y computed como practica

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
 
-  <Counter />
+  <Counter title='Batman'/>
+  
+  <Counter start="15"/>
   
 </template>
 
--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,14 +1,60 @@
 <template>
-  <h2>Counter</h2>
-  <p>8000</p>
+  <h2>{{title || 'Counter'}}</h2>
+  <p>{{counter}} <sup>2</sup> = {{squareCounter}} </p>
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ['title', 'start'],
+  data() {
+    return {
+      counter: this.start
+    }
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter * this.counter
+    },
+    // Increase
+    increase() {
+      this.counter++
+    },
+    // Decrease
+    decrease() {
+      this.counter--
+    },
 
+  },
+  computed: {
+    squareCounter() {
+      console.log('computed squareCounter')
+      return this.counter * this.counter
+    },
+    customTitle() {
+
+    }
+  }
 }
 </script>
 
 <style>
+button{
+  background-color: #64BB87;
+  border-radius: 5px;
+  padding: 5px 15px;
+  cursor: pointer;
+  border: 1px solid white;
+  color: white;
+  transition: 0.3s ease-in-out;
+  margin: 0 5px;
+}
 
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
 </style>
